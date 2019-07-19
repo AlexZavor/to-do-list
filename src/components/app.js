5df01642a68161f7db905153adec5c4a5d5043ee
AlexZavor <Enderdefender9@gmail.com>
2019-07-18 22:27:11 -0600

    website front page update

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,9 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Website startup</h1>
+        <h2>The very beggining of something new.</h2>
+        <p>Ill come back to this as soon as I can, weeeeeeee</p>
       </div>
     );
   }
