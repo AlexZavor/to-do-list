8f74dc0ff529f9943bd24aa0eee345ba649cbf39
AlexZavor <Enderdefender9@gmail.com>
2019-07-19 09:07:19 -0600

    set up routing

--- src/components/app.js
+++ src/components/app.js
@@ -11,6 +11,19 @@ export default class App extends Component {
     return (
       <div className='app'>
         <NavigationContainer />
+          <Router>
+            <div>
+              <Switch>
+                <Route exact path = "/" component = {Home} />
+                <Route path = "/about-me" component = {About} />
+                <Route path = "/contact" component = {Contact} />
+                <Route path = "/auth" component = {Auth} />
+                <Route path = "/blog" component = {Blog}/>
+
+                <Route component = {NoMatch} />
+              </Switch>
+            </div>
+          </Router>  
         <h1>Website startup</h1>
         <h2>The very beggining of something new.</h2>
         <p>Ill come back to this as soon as I can, weeeeeeee</p>
