c2f98c1fffb6b3caa73b393bab3f96e0cbd5733b
AlexZavor <Enderdefender9@gmail.com>
2019-07-19 09:11:43 -0600

    remove unneccicart parts from app

--- src/components/app.js
+++ src/components/app.js
@@ -28,11 +28,7 @@ export default class App extends Component {
                 <Route component = {NoMatch} />
               </Switch>
             </div>
-          </Router>  
-        <h1>Website startup</h1>
-        <h2>The very beggining of something new.</h2>
-        <p>Ill come back to this as soon as I can, weeeeeeee</p>
-      </div>
+          </Router>
     );
   }
 }
