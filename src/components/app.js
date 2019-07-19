7fdd1e99aa9d3b873ca98fd9914cd16be201228b
AlexZavor <Enderdefender9@gmail.com>
2019-07-19 08:37:06 -0600

    navbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import NavigationContainer from './navigation/navigation-container';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>Website startup</h1>
         <h2>The very beggining of something new.</h2>
         <p>Ill come back to this as soon as I can, weeeeeeee</p>
