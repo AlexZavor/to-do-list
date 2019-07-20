888a52f3a2a22d179b68e7fbc5269446e6bd1e01
AlexZavor <Enderdefender9@gmail.com>
2019-07-19 19:37:18 -0600

    store setup

--- src/components/pages/store.js
+++ src/components/pages/store.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import StoreItem from '../store/store-item'
 
 export default class Store extends Component {
     render() {
         return (
             <div>
                 <h1>Store Component</h1>
+
+                <StoreItem />
             </div>
         );
     }
---
+++ src/components/store/store-item.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class StoreItem extends Component{
+    render(){
+        return(
+            <div>
+                <h2>component 1</h2>
+            </div>
+        );
+    }
+}
