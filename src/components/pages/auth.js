8975f344ae43a709d83d277eb69fe8e093475626
AlexZavor <Enderdefender9@gmail.com>
2019-07-28 19:47:54 -0600

    fixed the component error

---
+++ src/components/pages/auth.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class auth extends Component {
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
+}
---
+++ src/components/pages/sign-up.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class room extends Component {
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
+}
